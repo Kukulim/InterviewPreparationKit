99febdd265221281ee475ea1f12e4b73289c7c70
Lukasz Bekus <beckon@interia.pl>
2019-11-06 17:11:21 +0100

    gut

--- InterviewPreparationKit.cpp
+++ InterviewPreparationKit.cpp
@@ -1,51 +1,31 @@
-﻿#include <iostream>
-#include<vector>
-
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int hourglassSum(vector<vector<int>> arr)
-{
-	int suma[16] = { 0 };
-	int p = 0;
-	int suma_max = 0;
-	int j = 0;
-	for (int w = 0; w < 4; w++)
+
+int main() {
+	long int N, K, p, q, sum, i, j, max = 0, x = 0;
+
+	cin >> N >> K;
+	long int* a = new long int[N + 1]();
+
+	for (i = 0; i < K; i++)
 	{
-		for (int k = 0; k < 4; k++)
-		{
-			for (int i = 0 + k; i < 3 + k; i++)
-			{
-				suma[p] += arr[j + w][i];
-				suma[p] += arr[j + 2 + w][i];
-			}
-			suma[p] += arr[1 + w][1 + k];
-			p++;
-		}
-	}
-	suma_max = suma[0];
-	for (int i = 0; i < 16; i++)
-		if (suma_max < suma[i])
-			suma_max = suma[i];
-
-	return suma_max;
-
-}
-
-int main()
-{
-
-	vector<vector<int>> arr(6);
-	for (int i = 0; i < 6; i++) {
-		arr[i].resize(6);
-
-		for (int j = 0; j < 6; j++) {
-			cin >> arr[i][j];
-		}
+		cin >> p >> q >> sum;
+		a[p] += sum;
+		if ((q + 1) <= N) a[q + 1] -= sum;
 	}
 
-	int result = hourglassSum(arr);
+	for (i = 1; i <= N; i++)
+	{
+		x = x + a[i];
+		if (max < x) max = x;
 
-	cout << result << "\n";
+	}
 
+	cout << max;
 	return 0;
 }
