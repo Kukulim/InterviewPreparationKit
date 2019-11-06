047a1a88046900653938125c4664283803ea82b4
Lukasz Bekus <beckon@interia.pl>
2019-11-06 15:30:26 +0100

    dziala ale za wolno z petla

--- InterviewPreparationKit.cpp
+++ InterviewPreparationKit.cpp
@@ -1,8 +1,38 @@
 ﻿#include <iostream>
+#include<string>
+
 using namespace std;
+
+// Complete the repeatedString function below.
+long repeatedString(string s, long n) {
+
+	long wynik = 0;
+	long licznik = 0;
+
+	while (licznik != n)
+	{
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == 'a') {
+				wynik++;
+			}
+			licznik++;
+			if (licznik == n)
+				break;
+
+		}
+	}
+	return wynik;
+}
 
 int main()
 {
+
+	string s;
+	cin >> s;
+	long n;
+	cin >> n;
+
+	cout<<  repeatedString(s, n);
 	return 0;
 }
-
