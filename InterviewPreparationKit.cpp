e8b26ce2e8c8b4e6074378d8a17d9c5774593820
Lukasz Bekus <beckon@interia.pl>
2019-11-06 13:41:26 +0100

    gut

--- InterviewPreparationKit.cpp
+++ InterviewPreparationKit.cpp
@@ -1,8 +1,31 @@
 ﻿#include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
+	int wielkosc;
+	cin >> wielkosc;
+	int* szuflada = new int[wielkosc];
+	for (int i = 0; i < wielkosc; i++)
+		cin >> szuflada[i];
+	int suma = 1;
+	int wynik = 0;
+	int buffor = -1;
+	for (int j = 0; j < wielkosc; j++)
+	{
+		for (int i = 1; i < wielkosc - j; i++)
+		{
+			if (szuflada[j] == szuflada[i + j])
+			{
+				szuflada[i + j] = buffor--;
+				suma++;
+			}
+		}
+		wynik += suma / 2;
+		suma = 1;
+	}
+	cout << wynik;
+	delete[]szuflada;
 	return 0;
 }
-
