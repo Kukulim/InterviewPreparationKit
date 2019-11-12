843d58cbc00c12b2ce53a13aaa89b5a333bdf7b1
Lukasz Bekus <beckon@interia.pl>
2019-11-12 15:18:15 +0100

    szukam mediany

--- InterviewPreparationKit.cpp
+++ InterviewPreparationKit.cpp
@@ -1,52 +1,28 @@
-﻿#include<cmath>
-#include<cstdio>
+﻿#include<iostream>
+#include<string>
 #include<vector>
-#include<iostream>
-#include<algorithm>
+
 using namespace std;
 
-struct Player {
-	int score;
-	string name;
-};
+int activityNotifications(vector<int> tablica, int d) {
+	int wynik=0;
 
-class Checker {
-public:
-	// complete this method
-	static int comparator(Player a, Player b) {
-		if (a.score > b.score)
-			return 1;
-		else if (a.score == b.score && a.name < b.name)
-			return 0;
-		else
-			return -1;
-	}
-};
-
-
-
-
-bool compare(Player a, Player b) {
-	if (Checker::comparator(a, b) == -1)
-		return false;
-	return true;
+	return wynik;
 }
+
 int main()
 {
-	int n;
-	cin >> n;
-	vector< Player > players;
-	string name;
-	int score;
+
+	int n, d;
+	cin >> n >> d;
+	vector<int> tablicaWynikow(n);
 	for (int i = 0; i < n; i++) {
-		cin >> name >> score;
-		Player player;
-		player.name = name, player.score = score;
-		players.push_back(player);
+
+		cin >> tablicaWynikow[i];
 	}
-	sort(players.begin(), players.end(), compare);
-	for (int i = 0; i < players.size(); i++) {
-		cout << players[i].name << " " << players[i].score << endl;
-	}
+	cout<< activityNotifications(tablicaWynikow, d);
+
+
 	return 0;
 }
+
