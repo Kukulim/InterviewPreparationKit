d7f5cc6ff42122f1ba2b61abd0e63ed96d8c36b9
Lukasz Bekus <beckon@interia.pl>
2019-11-14 13:48:47 +0100

    nowa proba

--- InterviewPreparationKit.cpp
+++ InterviewPreparationKit.cpp
@@ -5,47 +5,23 @@
 
 using namespace std;
 
-int activityNotifications(vector<int> &tablica, int d) {
-	int wynikBanku = 0;
-	int length;
-	length = tablica.size() - d;
-	for (int i = 0; i < length; i++)
-	{
-		vector<int>tabliczka(d);
-		for (int j = 0; j < d; j++)
-		{
-			tabliczka[j]=tablica[j+i];		
-		}
-		double wynik = 0;
-		double a = 0, b = 0;
-		sort(tabliczka.begin(), tabliczka.end());
-		if (tabliczka.size() % 2 != 0)
-			wynik = tabliczka[tabliczka.size() / 2];
-		else
-		{
-			a = tabliczka[tabliczka.size() / 2 - 1];
-			b = tabliczka[tabliczka.size() / 2];
-			wynik = (a + b) / 2;
-		}
-		if (wynik * 2 <= tablica[d+i])
-			wynikBanku++;
-	}
-	return wynikBanku;
+long countInversions(vector<int> arr) {
+
+
 }
 
 int main()
 {
-
-	int n, d;
-	cin >> n >> d;
-	vector<int> tablicaWynikow(n);
-	for (int i = 0; i < n; i++) {
-
-		cin >> tablicaWynikow[i];
+	int t;
+	cin >> t;
+	for (int t_itr = 0; t_itr < t; t_itr++) {
+		int n;
+		cin >> n;
+		vector<int> arr(n);
+		for (int i = 0; i < n; i++) {
+			cin>>arr[i];
+		}
+		cout << countInversions(arr);
 	}
-	cout<< activityNotifications(tablicaWynikow, d);
-
-
 	return 0;
 }
-
